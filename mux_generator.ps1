5d46b91f09cc38f0fe99ed389ab91e8beb347bcd
Peter Schuster <schuster.pe@gmail.com>
2014-01-07 02:59:28 +0100

    Compiled xsvf

---
+++ mux_generator.ps1
@@ -0,0 +1,27 @@
+﻿$i_max = 8
+$j_max = 60
+
+for ($i = 1; $i -le $i_max; $i++) {
+    $s = "he10($i) <="
+
+    $c = 0;
+    for ($j = 1; $j -le $j_max; $j++) {
+        $b = [convert]::ToString($j, 2)
+        if (($b.Length -ge $i) -and $b.substring($b.Length - $i, 1) -eq "1") {
+            if ($c -gt 0) {
+                $s += " or"
+            }
+
+            $s += " buttons($j)"
+            $c++;
+        }
+    }
+
+    if ($c -eq 0) {
+        $s += "'0'"
+    }
+
+    $s += ";"
+
+    echo $s
+}
